9462a77083788f2730edea3cc8333d93c90e5535
Slashh48 <pablodiablofun@gmail.com>
2023-07-18 20:24:59 +0200

    Vjezba treca full indzeksn

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Capricciousa",
-    toppings: ["Kechap", "Sir"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Shunka"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
   return (
     <>
-      <h1>{pizza.toppings + " "}</h1>
+      {cart.items.map((item) => item.quantity) + ""}
       <button onClick={handleClick}>Update</button>
     </>
   );
