43c937e6388f205f213e2a6df95ad1263ece7697
Slashh48 <pablodiablofun@gmail.com>
2023-07-19 18:20:03 +0200

    Dan vise ne znam koji, sto bi rekao veliki Muhammad Ali: Ne brojim ti ja trbusnjake ukupno, brojim tek kad pocne da boli.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <ExpandableText>Hello brate</ExpandableText>
-      <ExpandableText>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet vitae
-        fugiat nostrum animi alias impedit, explicabo soluta ab, assumenda
-        exercitationem nihil quos voluptate obcaecati incidunt aliquid
-        voluptatibus distinctio aut placeat. Asperiores officia quis harum,
-        consequatur quisquam cupiditate possimus rerum itaque veritatis, dolorum
-        corrupti porro doloribus aliquam ipsam ipsum sunt fugit, ab hic
-        molestiae vero! Minima voluptate quas magni ratione, sequi voluptatem
-        quae natus, commodi tempora soluta asperiores. Possimus non iusto, nam
-        aspernatur excepturi dolor voluptatibus adipisci eos alias ut? Quis
-        laboriosam accusantium fuga, cum blanditiis sint qui magni vero? Quos
-        reprehenderit illo atque cum distinctio, voluptatem doloribus
-        consequuntur nisi id.
-      </ExpandableText>
+      <Form />
     </>
   );
 }
