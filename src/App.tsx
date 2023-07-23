b8a0b0193ab3b36e746ec566141eb1a8377d2f62
Slashh48 <pablodiablofun@gmail.com>
2023-07-23 19:56:58 +0200

    Loading indikator - spinner

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,33 @@ function App() {
   const [error, setError] = useState("");
   const [users, setUsers] = useState<User[]>([]);
 
+  const [isLoading, setLoading] = useState(false);
+
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
+
+    //.finally(()=>{setLoading(false);});  -- ovo bih trebalo da radi ali ne radi u strict modu
 
     return () => controller.abort();
   }, []);
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
