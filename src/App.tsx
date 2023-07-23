09fa79df5ac7beb131b902095183329c1c5b72ab
Slashh48 <pablodiablofun@gmail.com>
2023-07-23 21:49:25 +0200

    Sibanje nedeljom. E a inace upravu sam bijo kad sam rekao u petak da je nedelju dana uchenja, no nijesam se odma na git i github povezo, a to ti ja ovako malo zbog si vi-ja malo  ; |

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ interface User {
 function App() {
   const [error, setError] = useState("");
   const [users, setUsers] = useState<User[]>([]);
-
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -33,13 +32,34 @@ function App() {
 
     return () => controller.abort();
   }, []);
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
