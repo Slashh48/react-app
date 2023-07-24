1fb785f37a3830ab71909752d5a47c1fe91feef7
Slashh48 <pablodiablofun@gmail.com>
2023-07-24 21:48:21 +0200

    Evo gotovoooo sa vjezbaamaaaaa. A sad: projekt.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import { CanceledError } from "./services/api-client";
 import { useEffect, useState } from "react";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [error, setError] = useState("");
-  const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    //.finally(()=>{setLoading(false);});  -- ovo bih trebalo da radi ali ne radi u strict modu
-
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
